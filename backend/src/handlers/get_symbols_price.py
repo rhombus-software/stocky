69342eef72956f72c4884c25f4bf84c26186fce4
rhombus-india <153076114+rhombus-india@users.noreply.github.com>
2026-06-23 14:01:24 +0000

    updated

--- backend/src/handlers/get_symbols_price.py
+++ backend/src/handlers/get_symbols_price.py
@@ -3,12 +3,23 @@ import yfinance as yf
 def get_symbols_price(symbols):
     print("Fetching details from yfinance for symbols:", symbols)
     prices = {}
+    tick = yf.download(symbols, period="2d", interval="1d")['Close']
+    print(tick)
     for symbol in symbols:
         try:
             ticker = yf.Ticker(symbol)
-            price = ticker.fast_info['last_price']
-            print(f"Retrieved price for {symbol}: {price}")
-            prices[symbol] = price
+            current_price = ticker.fast_info['last_price']
+            print(current_price)
+            print(f"Retrieved price for {symbol}: {current_price}")
+            prev_day = tick[symbol].iloc[0]
+            print(prev_day)
+            change = ((current_price - prev_day) / prev_day) * 100
+            print(change)
+            prices[symbol] = {
+                "current_price": current_price,
+                "change": change
+            }
+
         except Exception as e:
             print(f"Error occurred while fetching price for {symbol}: {e}")
             prices[symbol] = None  # Handle errors gracefully
